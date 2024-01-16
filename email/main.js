ee90c40af68969b797253a3c7641504c06311ff4
Murru00 <marcomurruolbia@gmail.com>
2024-01-17 00:48:33 +0100

    risultato dadi

--- email/main.js
+++ email/main.js
@@ -10,3 +10,13 @@ alert('hai tirato ' + guestNumber);
 
 const computerNumber = Math.floor(Math.random() * 6 + 1);
 alert('il pc ha tirato ' + computerNumber);
+
+//UTENTE E PC TIRANO I DADI E VINCE CHI HA IL NUMERO PIU' ALTO
+
+if (guestNumber > computerNumber) {
+    resultElement.innerHTML = ('hai vinto tu');
+} else if (guestNumber < computerNumber) {
+    resultElement.innerHTML = ('ha vinto il pc');
+} else if (guestNumber == computerNumber) {
+    resultElement.innerHTML = ('pareggio');
+}
