9fd50de24e8e9ca5a70d19d30de17f248fa124a8
Murru00 <marcomurruolbia@gmail.com>
2024-01-17 00:45:07 +0100

    add random number

--- email/main.js
+++ email/main.js
@@ -1,3 +1,12 @@
-const resultElement = document.getElementById{'result'}
+const resultElement = document.getElementById('result');
 
-resultElement.innerHTML = ('Sei pronto a giocare?')
+resultElement.innerHTML = ('Sei pronto a giocare?');
+
+
+//GENERO UN NUMERO RANDOM TRA 1 E 6
+
+const guestNumber = Math.floor(Math.random() * 6 + 1);
+alert('hai tirato ' + guestNumber);
+
+const computerNumber = Math.floor(Math.random() * 6 + 1);
+alert('il pc ha tirato ' + computerNumber);
